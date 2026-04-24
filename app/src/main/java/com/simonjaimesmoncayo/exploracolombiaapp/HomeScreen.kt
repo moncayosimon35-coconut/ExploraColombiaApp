1e0175ace3e8861203af7c3791edc5163b0aeb1d
moncayosimon35-coconut <moncayosimon35@gmail.com>
2026-04-24 01:36:11 -0500

    Pantalla Home y Botón Cerrar Sesión Register 24 Abril 2026

--- app/src/main/java/com/simonjaimesmoncayo/exploracolombiaapp/HomeScreen.kt
+++ app/src/main/java/com/simonjaimesmoncayo/exploracolombiaapp/HomeScreen.kt
@@ -1,2 +1,49 @@
 package com.simonjaimesmoncayo.exploracolombiaapp
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+@Composable
+fun HomeScreen(onLogout: () -> Unit) {
+    val auth = Firebase.auth
+    val user = auth.currentUser
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "HOME SCREEN", fontSize = 30.sp)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (user != null) {
+                // mostrar correo usuario
+                Text(text = user.email.toString(), fontSize = 18.sp, color = Color.Gray)
+            } else {
+                Text(text = "No hay usuario", color = Color.Red)
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // boton para cerrar sesion
+            Button(
+                onClick = {
+                    auth.signOut() // 1. Cierra sesión en Firebase
+                    onLogout()      // 2. Avisa al NavHost para que nos mande al Login
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+            ) {
+                Text("Cerrar Sesión", color = Color(0xFFFF9900)) // El color naranja de tu imagen
+            }
+        }
+    }
+}
--- app/src/main/java/com/simonjaimesmoncayo/exploracolombiaapp/MainActivity.kt
+++ app/src/main/java/com/simonjaimesmoncayo/exploracolombiaapp/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.simonjaimesmoncayo.exploracolombiaapp.ui.theme.ExploraColombiaAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +27,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExploraColombiaAppTheme {
                 val myNavController = rememberNavController()
+                //mirar si el usuairo esta logueado o no
+                val auth = Firebase.auth
+                val currentUser = auth.currentUser
+                //ruta inicial no se pierda
+                var myStartDestination by remember { mutableStateOf("login") }
+
+                if (currentUser != null) {
+                    myStartDestination = "home"
+                } else {
+                    myStartDestination = "login"
+                }
                 NavHost(
                     navController = myNavController,
-                    startDestination = "login",
+                    startDestination = myStartDestination,
                     modifier = Modifier.fillMaxSize()
                 ) {
                     composable(route = "login") {
@@ -57,10 +74,14 @@ class MainActivity : ComponentActivity() {
                     }
 
                     composable(route = "home") {
-                        // pantalla de Home vacía por ahora
-                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                            Text("¡Bienvenido a Explora Colombia!")
-                        }
+                        HomeScreen(
+                            onLogout = {
+                                // la persona vuelve a login y se borra todo el historial porque pues cerro sesion
+                                myNavController.navigate("login") {
+                                    popUpTo("home") { inclusive = true }
+                                }
+                            }
+                        )
                     }
 
                 }
